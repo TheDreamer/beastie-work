41017ed302730688d087a866d69b1028006c1798
The Dreamer <beastie@tardisi.com>
2014-04-29 09:38:05 -0500

    * Update for v2.2.12

--- patches/mail/dovecot2/files/patch-src__plugins__fts-lucene__SnowballFilter.h
+++ patches/mail/dovecot2/files/patch-src__plugins__fts-lucene__SnowballFilter.h
@@ -1,5 +1,5 @@
---- src/plugins/fts-lucene/SnowballFilter.h.orig	2012-09-24 15:21:29.000000000 -0500
-+++ src/plugins/fts-lucene/SnowballFilter.h	2013-02-03 11:24:06.647137581 -0600
+--- src/plugins/fts-lucene/SnowballFilter.h.orig	2013-03-14 08:28:31.000000000 -0500
++++ src/plugins/fts-lucene/SnowballFilter.h	2014-04-29 09:30:51.625055519 -0500
 @@ -8,7 +8,7 @@
  #define _lucene_analysis_snowball_filter_
  
